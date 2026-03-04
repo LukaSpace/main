5ef7d51a38f51fea5d4ca7438f0013543f4aa962
LC <mclukinas765@gmail.com>
2026-03-04 22:20:39 +0100

    adjust styles, add comercial projects

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -44,12 +44,43 @@ export class PortfolioComponent {
 
   comercialProjects: Project[] = [
     {
+      title: 'Annual Energy Production Calculation Service',
+      description:
+        'A web application designed to calculate the annual energy production of a wind turbine park. ' +
+        'The application takes into account various factors such as wind speed, turbine specifications, and environmental conditions to provide accurate energy production estimates. ' +
+        'The application also includes features for data visualization and reporting.',
+      technologies: [
+        '.NET Core',
+        'Angular',
+        'Azure CD/CI',
+        'C#',
+        'EF Core',
+        'Hangfire',
+        'JSON Parsing',
+        'JWT Authentication',
+        'MVC',
+        'MS SQL',
+        'Plotly.js',
+        'Rest API',
+        'Services Architecture',
+        'XUnit',
+      ],
+    },
+    {
       title: 'Products Portfolio',
       description:
         'A web application showcasing a range of commercial products with detailed descriptions and images. ' +
         'The application read data from exported .xml files, parse them and display the information in a user-friendly interface. ' +
         'All data are stored in a MS SQL database and refresh each day using a scheduled job.',
-      technologies: ['.NET Core', 'C#', 'EF Core', 'MS SQL', 'XUnit', 'Angular', 'Rest API', 'Hangfire', 'Animations', 'Azure CD/CI'],
+      technologies: ['.NET Core', 'Angular', 'Animations', 'Azure CD/CI', 'C#', 'EF Core', 'Hangfire', 'JWT Authentication', 'MS SQL', 'MVC', 'Rest API', 'Tailwind CSS', 'XML Parsing', 'XUnit'],
+    },
+    {
+      title: 'Migration of Legacy Application',
+      description:
+        'A project focused on migrating a legacy VB application to a modern technology stack. ' +
+        'The migration process involved analyzing the existing application, designing a new architecture, and implementing the new application using Angular 21 and .NET 10. with Telerik UI. ' +
+        'The project also included data migration, testing, and supporting old project files according to stackholders requirements.',
+      technologies: ['.NET Core', 'C#', 'MVVM', 'MVVM Toolkit', 'Math.NET Numerics', 'MiniExcel', 'Siemens NX Open API', 'Telerik UI', 'WPF', 'XUnit'],
     },
   ];
 
