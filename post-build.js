399aa34c64d037c94283d16ac42b07efb5f79659
LC <mclukinas765@gmail.com>
2026-03-18 23:30:34 +0100

    adjust post build

--- post-build.js
+++ post-build.js
@@ -1,5 +1,10 @@
 const fs = require('fs-extra');
-fs.move('docs/browser', 'docs', { overwrite: true }, err => {
+fs.copySync('docs/browser', 'docs', { overwrite: true }, err => {
+  if (err) {
+    return console.error(err);
+  }
+});
+fs.removveSync('docs/browser', err => {
   if (err) {
     return console.error(err);
   }
